957aeeef26de9e195dd2d9e35108c3afdd909452
CrownKira <t.karwi@yahoo.com>
2020-12-15 22:18:22 +0800

    Added CirclesCanvas

--- src/components/CirclesCanvas.js
+++ src/components/CirclesCanvas.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import '../style/CirclesCanvas.css';
+import loadScript from '../loadScript';
 
-const CirclesCanvas = () => {
+class CirclesCanvas extends React.Component {
     //:anything => route parameter   
     //</Switch> only show one route
-    return (
-        <div>
-            Circle Canvas
-        </div>
+    componentDidMount() {
+        loadScript('/externalLibs/env_visualizer/draw_circles.js');
+    }
 
-    );
+    render() {
+        return (
+            <>
+                <div id="controls">
+                    <button id="moveUp">Move selected up</button>
+                    <button id="moveDown">Move selected down</button>
+                    <button id="moveToTop">Move selected to top</button>
+                    <button id="moveToBottom">Move selected to bottom</button>
+                    <button id="destroy">Destroy selected</button>
+                    <button id="download">Download</button>
+                </div>
+                <div id="concreteContainer">
+                </div>
+            </>
+
+        );
+    }
 };
 
 export default CirclesCanvas;
