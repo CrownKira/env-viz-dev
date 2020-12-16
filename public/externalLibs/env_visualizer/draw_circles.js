3b1fc566a731b518b3b04d831245f4c0bdec8a26
CrownKira <t.karwi@yahoo.com>
2020-12-16 17:41:18 +0800

    Fixed issues with Circles Canvas

--- public/externalLibs/env_visualizer/draw_circles.js
+++ public/externalLibs/env_visualizer/draw_circles.js
@@ -1,246 +1,248 @@
 /*
  * THREE CIRCLES EXAMPLE FROM http://www.concretejs.com/
  */
-function drawSceneCircles() {
-    circles.forEach(function (circle) {
-        drawSceneCircle(circle);
-    });
-    viewport.render();
-}
-
-function drawHitCircles() {
-    circles.forEach(function (circle) {
-        drawHitCircle(circle);
-    });
-}
-
-function drawSceneCircle(config) {
-    var scene = config.layer.scene,
-        context = scene.context;
-
-    scene.clear();
-    context.save();
-    context.beginPath();
-    context.arc(config.x, config.y, 60, 0, Math.PI * 2, false);
-    context.fillStyle = config.color;
-    context.fill();
-
-    if (config.selected) {
-        context.strokeStyle = 'black';
-        context.lineWidth = 6;
-        context.stroke();
+(function () {
+    function drawSceneCircles() {
+        circles.forEach(function (circle) {
+            drawSceneCircle(circle);
+        });
+        viewport.render();
     }
 
-    if (config.hovered) {
-        context.strokeStyle = 'green';
-        context.lineWidth = 2;
-        context.stroke();
+    function drawHitCircles() {
+        circles.forEach(function (circle) {
+            drawHitCircle(circle);
+        });
     }
-    context.restore();
-}
 
-function drawHitCircle(config) {
-    var hit = config.layer.hit,
-        context = hit.context;
+    function drawSceneCircle(config) {
+        var scene = config.layer.scene,
+            context = scene.context;
 
-    hit.clear();
-    context.save();
-    context.beginPath();
-    context.arc(config.x, config.y, 60, 0, Math.PI * 2, false);
-    context.fillStyle = hit.getColorFromIndex(config.key);
-    context.fill();
-    context.restore();
-}
+        scene.clear();
+        context.save();
+        context.beginPath();
+        context.arc(config.x, config.y, 60, 0, Math.PI * 2, false);
+        context.fillStyle = config.color;
+        context.fill();
 
-function getCircleFromKey(key) {
-    var len = circles.length,
-        n, circle;
-
-    for (n = 0; n < len; n++) {
-        circle = circles[n];
-        if (circle.key === key) {
-            return circle;
+        if (config.selected) {
+            context.strokeStyle = 'black';
+            context.lineWidth = 6;
+            context.stroke();
         }
-    }
 
-    return null;
-}
-
-function getSelectedCircle() {
-    var len = circles.length,
-        n, circle;
-
-    for (n = 0; n < len; n++) {
-        circle = circles[n];
-        if (circle.selected) {
-            return circle;
+        if (config.hovered) {
+            context.strokeStyle = 'green';
+            context.lineWidth = 2;
+            context.stroke();
         }
+        context.restore();
     }
 
-    return null;
-}
+    function drawHitCircle(config) {
+        var hit = config.layer.hit,
+            context = hit.context;
 
-function destroyCircle(key) {
-    var len = circles.length,
-        n, circle;
+        hit.clear();
+        context.save();
+        context.beginPath();
+        context.arc(config.x, config.y, 60, 0, Math.PI * 2, false);
+        context.fillStyle = hit.getColorFromIndex(config.key);
+        context.fill();
+        context.restore();
+    }
 
-    for (n = 0; n < len; n++) {
-        circle = circles[n];
-        if (circle.key === key) {
-            circles.splice(n, 1);
-            drawSceneCircles();
-            drawHitCircles();
-            return true;
+    function getCircleFromKey(key) {
+        var len = circles.length,
+            n, circle;
+
+        for (n = 0; n < len; n++) {
+            circle = circles[n];
+            if (circle.key === key) {
+                return circle;
+            }
         }
+
+        return null;
     }
 
-    return false;
-}
+    function getSelectedCircle() {
+        var len = circles.length,
+            n, circle;
 
-var concreteContainer = document.getElementById('concreteContainer');
+        for (n = 0; n < len; n++) {
+            circle = circles[n];
+            if (circle.selected) {
+                return circle;
+            }
+        }
 
-// create viewport
-var viewport = new Concrete.Viewport({
-    width: 400,
-    height: 200,
-    container: concreteContainer
-});
-
-// create layers
-var layer1 = new Concrete.Layer();
-var layer2 = new Concrete.Layer();
-var layer3 = new Concrete.Layer();
-
-// add layers
-viewport.add(layer1).add(layer2).add(layer3);
-
-// create a scene graph (you don't need a scene graph for Concrete, but in this
-// example we will use one
-var circles = [
-    {
-        x: viewport.width / 2,
-        y: viewport.height * 2 / 3,
-        hovered: false,
-        selected: true,
-        layer: layer1,
-        color: 'red',
-        key: 0
-    },
-    {
-        x: viewport.width * 2 / 5,
-        y: viewport.height / 3,
-        hovered: false,
-        selected: false,
-        layer: layer2,
-        color: 'yellow',
-        key: 1
-    },
-    {
-        x: viewport.width * 3 / 5,
-        y: viewport.height / 3,
-        hovered: false,
-        selected: false,
-        layer: layer3,
-        color: 'orange',
-        key: 2
+        return null;
     }
-];
 
-// draw circles onto hit canvases for mouse detection
-drawHitCircles();
+    function destroyCircle(key) {
+        var len = circles.length,
+            n, circle;
 
-// draw visible circles
-drawSceneCircles();
+        for (n = 0; n < len; n++) {
+            circle = circles[n];
+            if (circle.key === key) {
+                circles.splice(n, 1);
+                drawSceneCircles();
+                drawHitCircles();
+                return true;
+            }
+        }
 
-// add concrete container handlers
-concreteContainer.addEventListener('mousemove', function (evt) {
-    var boundingRect = concreteContainer.getBoundingClientRect(),
-        x = evt.clientX - boundingRect.left,
-        y = evt.clientY - boundingRect.top,
-        key = viewport.getIntersection(x, y),
-        circle;
+        return false;
+    }
 
-    // unhover all circles
-    circles.forEach(function (circle) {
-        circle.hovered = false;
+    var concreteContainer = document.getElementById('concreteContainer');
+
+    // create viewport
+    var viewport = new Concrete.Viewport({
+        width: 400,
+        height: 200,
+        container: concreteContainer
     });
 
-    if (key >= 0) {
-        circle = getCircleFromKey(key);
-        circle.hovered = true;
-    }
+    // create layers
+    var layer1 = new Concrete.Layer();
+    var layer2 = new Concrete.Layer();
+    var layer3 = new Concrete.Layer();
 
+    // add layers
+    viewport.add(layer1).add(layer2).add(layer3);
+
+    // create a scene graph (you don't need a scene graph for Concrete, but in this
+    // example we will use one
+    var circles = [
+        {
+            x: viewport.width / 2,
+            y: viewport.height * 2 / 3,
+            hovered: false,
+            selected: true,
+            layer: layer1,
+            color: 'red',
+            key: 0
+        },
+        {
+            x: viewport.width * 2 / 5,
+            y: viewport.height / 3,
+            hovered: false,
+            selected: false,
+            layer: layer2,
+            color: 'yellow',
+            key: 1
+        },
+        {
+            x: viewport.width * 3 / 5,
+            y: viewport.height / 3,
+            hovered: false,
+            selected: false,
+            layer: layer3,
+            color: 'orange',
+            key: 2
+        }
+    ];
+
+    // draw circles onto hit canvases for mouse detection
+    drawHitCircles();
+
+    // draw visible circles
     drawSceneCircles();
-});
 
-concreteContainer.addEventListener('click', function (evt) {
-    var boundingRect = concreteContainer.getBoundingClientRect(),
-        x = evt.clientX - boundingRect.left,
-        y = evt.clientY - boundingRect.top,
-        key = viewport.getIntersection(x, y),
-        circle;
+    // add concrete container handlers
+    concreteContainer.addEventListener('mousemove', function (evt) {
+        var boundingRect = concreteContainer.getBoundingClientRect(),
+            x = evt.clientX - boundingRect.left,
+            y = evt.clientY - boundingRect.top,
+            key = viewport.getIntersection(x, y),
+            circle;
 
-    // unhover all circles
-    circles.forEach(function (circle) {
-        circle.selected = false;
+        // unhover all circles
+        circles.forEach(function (circle) {
+            circle.hovered = false;
+        });
+
+        if (key >= 0) {
+            circle = getCircleFromKey(key);
+            circle.hovered = true;
+        }
+
+        drawSceneCircles();
     });
 
-    if (key >= 0) {
-        circle = getCircleFromKey(key);
-        circle.selected = true;
-    }
+    concreteContainer.addEventListener('click', function (evt) {
+        var boundingRect = concreteContainer.getBoundingClientRect(),
+            x = evt.clientX - boundingRect.left,
+            y = evt.clientY - boundingRect.top,
+            key = viewport.getIntersection(x, y),
+            circle;
 
-    drawSceneCircles();
-});
+        // unhover all circles
+        circles.forEach(function (circle) {
+            circle.selected = false;
+        });
 
-// add button handlers
-document.getElementById('moveUp').addEventListener('click', function () {
-    var circle = getSelectedCircle();
+        if (key >= 0) {
+            circle = getCircleFromKey(key);
+            circle.selected = true;
+        }
 
-    if (circle) {
-        circle.layer.moveUp();
-    }
-    viewport.render();
-});
-
-document.getElementById('moveDown').addEventListener('click', function () {
-    var circle = getSelectedCircle();
-
-    if (circle) {
-        circle.layer.moveDown();
-    }
-    viewport.render();
-});
-
-document.getElementById('moveToTop').addEventListener('click', function () {
-    var circle = getSelectedCircle();
-
-    if (circle) {
-        circle.layer.moveToTop();
-    }
-    viewport.render();
-});
-
-document.getElementById('moveToBottom').addEventListener('click', function () {
-    var circle = getSelectedCircle();
-
-    if (circle) {
-        circle.layer.moveToBottom();
-    }
-    viewport.render();
-});
-
-document.getElementById('destroy').addEventListener('click', function () {
-    var circle = getSelectedCircle();
-
-    if (circle) {
-        circle.layer.destroy();
-    }
-    viewport.render();
-});
-
-document.getElementById('download').addEventListener('click', function () {
-    viewport.scene.download({
-        fileName: 'three-circles.png'
+        drawSceneCircles();
     });
-});
+
+    // add button handlers
+    document.getElementById('moveUp').addEventListener('click', function () {
+        var circle = getSelectedCircle();
+
+        if (circle) {
+            circle.layer.moveUp();
+        }
+        viewport.render();
+    });
+
+    document.getElementById('moveDown').addEventListener('click', function () {
+        var circle = getSelectedCircle();
+
+        if (circle) {
+            circle.layer.moveDown();
+        }
+        viewport.render();
+    });
+
+    document.getElementById('moveToTop').addEventListener('click', function () {
+        var circle = getSelectedCircle();
+
+        if (circle) {
+            circle.layer.moveToTop();
+        }
+        viewport.render();
+    });
+
+    document.getElementById('moveToBottom').addEventListener('click', function () {
+        var circle = getSelectedCircle();
+
+        if (circle) {
+            circle.layer.moveToBottom();
+        }
+        viewport.render();
+    });
+
+    document.getElementById('destroy').addEventListener('click', function () {
+        var circle = getSelectedCircle();
+
+        if (circle) {
+            circle.layer.destroy();
+        }
+        viewport.render();
+    });
+
+    document.getElementById('download').addEventListener('click', function () {
+        viewport.scene.download({
+            fileName: 'three-circles.png'
+        });
+    });
+})()
