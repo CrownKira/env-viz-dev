ef920d8b8826fd94b4ed0b9edab63f8f100943b5
CrownKira <t.karwi@yahoo.com>
2021-01-24 19:02:56 +0800

    Shift loadLibs functions to externalLibs directory

---
+++ public/externalLibs/index.js
@@ -0,0 +1,34 @@
+function dynamicallyLoadScript(url) {
+  var script = document.createElement('script');
+  script.src = url;
+  // Forces scripts to be loaded in order
+  script.async = false;
+  script.defer = true;
+  // make sure document.body exists, since the scripts we load
+  // assume that it does
+  if (document.body) {
+    document.body.appendChild(script);
+  } else {
+    var observer = new MutationObserver(function () {
+      if (document.body) {
+        document.body.appendChild(script);
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.documentElement, { childList: true });
+  }
+}
+
+function loadAllLibs() {
+  const files = [
+    // env visualizer
+    '/externalLibs/env_visualizer/ConcreteJs.js',
+    '/externalLibs/env_visualizer/visualizer.js'
+  ];
+
+  for (var i = 0; i < files.length; i++) {
+    dynamicallyLoadScript(files[i]);
+  }
+}
+
+loadAllLibs();
