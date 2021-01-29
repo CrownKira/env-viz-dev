d69286c2a451409c0154256b4f4ed588b68e6644
CrownKira <t.karwi@yahoo.com>
2021-01-29 18:49:04 +0800

    Add type param to useState

--- src/utils/forceUpdate.ts
+++ src/utils/forceUpdate.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
 export default function useForceUpdate(): () => void {
-  const [, setValue] = useState(0);
+  const [, setValue] = useState<number>(0);
   return () => setValue(value => value + 1);
 }
