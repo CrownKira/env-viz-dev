efea323729dc9d99060ef58212906e1175f367bd
CrownKira <t.karwi@yahoo.com>
2021-02-13 22:02:35 +0800

    Add dimensions logic to Level class

--- src/drawers/components/Level.tsx
+++ src/drawers/components/Level.tsx
@@ -1,16 +1,27 @@
 import { Frame } from './Frame';
 import { Drawable } from '../types';
+import { Dimension } from '../Dimension';
 
 /** this class encapsulates a level of frames to be drawn with the same y values */
 export class Level implements Drawable {
-  constructor(readonly frames: Frame[]) {
+  readonly x: number;
+  readonly y: number;
+  readonly height: number;
+  readonly width: number;
+
+  constructor(
+    readonly frames: Frame[],
+    /** the parent level of this level (the level above it) */
+    readonly parentLevel: Level | null
+  ) {
     // get the max height of all the frames in this level
-    const height = frames.reduce<number>(
-      (maxHeight, frame) => Math.max(maxHeight, frame.height),
-      0
-    );
+    /// frame already constructed
+    this.height = frames.reduce<number>((maxHeight, frame) => Math.max(maxHeight, frame.height), 0);
     // get the total width of all the frames in this level
-    const width = frames.reduce<number>((totalWidth, frame) => (totalWidth += frame.width + 0), 0);
+    this.width = frames.reduce<number>((totalWidth, frame) => (totalWidth += frame.width + 0), 0);
+    this.x = Dimension.MarginLeft;
+    this.y = Dimension.MarginTop;
+    parentLevel && (this.y += parentLevel.height + parentLevel.y);
   }
 
   draw() {
