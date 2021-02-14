74f6e1ae16032fae9b281c2059debe356d3f1477
CrownKira <t.karwi@yahoo.com>
2021-02-14 09:15:25 +0800

    Create values in Binding constructor

--- src/drawers/components/binding/Binding.tsx
+++ src/drawers/components/binding/Binding.tsx
@@ -1,23 +1,60 @@
 import { Frame } from '../Frame';
-import { Drawable } from '../../types';
+import { Visible, Data } from '../../types';
 import { Value } from './Value';
+import { Text } from '../Text';
+import { PrimitiveValue } from './value/PrimitiveValue';
+import { ArrayValue } from './value/ArrayValue';
+import { Dimension } from '../../Dimension';
+import { Layout } from '../../Layout';
 
 /** a `binding` is a key-value pair in a frame */
-export class Binding implements Drawable {
+export class Binding implements Visible {
   /** the text to render */
-  readonly text: Text;
+  readonly x: number;
+  readonly y: number;
+  readonly width: number;
+  readonly height: number;
+  readonly name: Text;
+  readonly value: Value;
+  readonly isInFrame: boolean;
 
   constructor(
     /** the key of this binding */
-    readonly key: string,
+    readonly key: string, /// the name!
     /** the value of this binding */
-    readonly value: Value,
+    readonly data: Data,
     /** frame this binding is in */
-    readonly frame: Frame
+    readonly frame: Frame,
+    readonly prevBinding: Binding | null
   ) {
-    value.referencedBy.push(this);
-    this.text = new Text(key);
+    // const value = Layout.createValue(data as Data, frame);
+    // if (value instanceof ArrayValue) value.init();
+
+    if (prevBinding) {
+      this.x = prevBinding.name.x;
+      this.y = prevBinding.name.y + prevBinding.name.height + Dimension.TextPaddingY;
+    } else {
+      this.x = this.frame.x + Dimension.TextPaddingX;
+      this.y = this.frame.y + Dimension.TextPaddingY;
+    }
+    this.name = new Text(key, this.x, this.y);
+
+    const value = Layout.createValue(data, this.frame, this);
+    if (value instanceof ArrayValue) value.init();
+    // value.referencedBy.push(this);
+    this.value = value;
+    this.width = this.value.x + this.value.width - this.x;
+    this.height = Math.max(this.name.height, this.value.height);
+
+    // this.value.x = this.x;
+    // this.value.y = this.y;
+    this.isInFrame = value instanceof PrimitiveValue;
   }
+
+  // private setValueDimension():void {
+  //   this.value.x = this.x;
+  //   this.value.y = this.y;
+  // }
 
   draw() {
     return <></>;
