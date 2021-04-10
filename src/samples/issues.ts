00d8a972e258cecd7f1717955538d6b39792e9e3
CrownKira <t.karwi@yahoo.com>
2021-04-10 21:55:15 +0800

    Update indentations to issues.ts

--- src/samples/issues.ts
+++ src/samples/issues.ts
@@ -5,7 +5,7 @@ const ISSUES = [
 let x = 0;
 let y = 10;
 function f(x) {
-return () => 6; //arrow pointing to the wrong frame
+  return () => 6; //arrow pointing to the wrong frame
 }
 const z = f(11);
 debugger;
@@ -17,11 +17,11 @@ debugger;
 let x = 0;
 let y = 10;
 function f(x) {
-function g(x) {
-x = x + 1;
-y = y + 1;
-}
-return g;
+  function g(x) {
+    x = x + 1;
+    y = y + 1;
+  }
+  return g;
 }
 const z = f(11);
 debugger; 
@@ -31,10 +31,10 @@ debugger;
     description: 'Empty frame should not be drawn',
     code: `/* Fixed */
 function longfn(a) {
-a = (x) => {
-debugger;
-};
-a(10);
+  a = (x) => {
+    debugger;
+  };
+  a(10);
 }
 longfn(undefined);
 `
@@ -43,11 +43,11 @@ longfn(undefined);
     description: 'Frame missing',
     code: `/* Fixed */
 function fn0() {
-const a = fn1(10);
-debugger;
+  const a = fn1(10);
+  debugger;
 }
 function fn1(x) {
-return () => 1;
+  return () => 1;
 }
 fn0();
 `
@@ -72,13 +72,12 @@ debugger;
     description: 'Frame spacing too big',
     code: `/* Fixed */
 function fib(n) {
-if (n<=1) {
-return n;
-} else {
-debugger;
-return fib(n-1)+fib(n-2);
-
-}
+  if (n<=1) {
+    return n;
+  } else {
+    debugger;
+    return fib(n-1)+fib(n-2);
+  }
 }
 fib(5);
 `
@@ -105,7 +104,7 @@ debugger;`
     description: '',
     code: `let x = null;
 for (let i = 0; i < 5; i = i + 1) {
-x = pair(() => i, x);
+  x = pair(() => i, x);
 }
 
 debugger;`
@@ -113,12 +112,12 @@ debugger;`
   {
     description: 'Frame Width Calculation Issue',
     code: `function f(n) {
-if (n <= 1) {
-debugger;
-return 1;
-} else {
-return n * f(n-1);
-}
+  if (n <= 1) {
+    debugger;
+  return 1;
+  } else {
+    return n * f(n-1);
+  }
 }
 
 f(5);`
